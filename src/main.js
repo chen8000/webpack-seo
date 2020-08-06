ed8106d56b56d932b927ad2335b9760db4d275d5
chen <chenzhanghui@yplsec.com>
2020-08-06 16:59:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-07-16 18:46:56
  * @LastEditors: chenzhanghui
- * @LastEditTime: 2020-08-06 12:14:19
+ * @LastEditTime: 2020-08-06 16:23:46
  */ 
 import Vue from 'vue'
 import App from './app.vue'
@@ -10,7 +10,7 @@ import router from './route'
 new Vue({
   render: h => h(App),
   router,
-  mounted() {
-    document.dispatchEvent(new Event('custom-render-trigger'))
-  }
+  // mounted() {
+  //   document.dispatchEvent(new Event('custom-render-trigger'))
+  // }
 }).$mount('#app')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2020-07-16 18:47:40
  * @LastEditors: chenzhanghui
- * @LastEditTime: 2020-08-06 12:13:43
+ * @LastEditTime: 2020-08-06 16:58:49
  */ 
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
@@ -9,8 +9,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
 const rules = require('./webpack.rules')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 module.exports = {
   mode: 'development', // "production" | "development" | "none"
@@ -31,23 +29,6 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(), // 模块热替换
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    }),
-    new PrerenderSPAPlugin({
-      staticDir: path.join(__dirname, './dist'), 
-      // outputDir: path.join(__dirname, './public'),
-      // indexPath: path.join(__dirname, './public', 'index.html'), 
-      routes: ['/route', '/test'],
-      
-      renderer: new Renderer({
-        ignoreJSErrors: true,
-        inject: {
-          foo: 'bar'
-        },
-        headless: false,
-        // 在 main.js 中 document.dispatchEvent(new Event('render-event'))，两者的事件名称要对应上。
-        // renderAfterDocumentEvent: 'render-event',
-        renderAfterTime: 5000
-      })
     })
   ],
   resolve: {
